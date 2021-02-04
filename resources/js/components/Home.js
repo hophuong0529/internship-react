f6669da598a70e2418a130474c4ee70c34081376
hophuong0529 <hophuong0529@gmail.com>
2021-02-04 11:32:58 +0700

    home page

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Col, Container, Row} from "react-bootstrap";
-import Left from "./Left";
+import Categories from "./Categories";
 import Carousel from "./Carousel";
 import LatestProduct from "./LatestProduct";
 import TopProduct from "./TopProduct";
@@ -12,7 +12,7 @@ class Home extends Component {
                 <Container fluid style={{paddingTop: 0}}>
                     <Row style={{paddingRight: '10px'}}>
                         <Col md={3} style={{paddingRight: 0}}>
-                            <Left/>
+                            <Categories/>
                         </Col>
                         <Col md={9}>
                             <Carousel/>
